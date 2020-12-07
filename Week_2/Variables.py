a4b9ce76a43d8497bccda3036f575e23ea1b9fc2
ChristianJdeVries <christiandevries8@gmail.com>
2020-12-07 13:34:07 +0100

    versioon 1

---
+++ Week_2/Variables.py
@@ -0,0 +1,21 @@
+a = "All"
+b = "work"
+c = "and"
+d = "no"
+e = "play"
+f = "makes"
+g = "Jack"
+h = "a"
+i = "dull"
+j = "boy"
+print(a,b,c,d,e,f,g,h,i,j)
+
+print(6*1-2)
+print(6*(1-2))
+
+bruce = 6
+bruce + 4
+print(bruce + 4)
+
+
+
