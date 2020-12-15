3aef6a879020b9b6bd922e31ec8cd31fbd7f953c
ChristianJdeVries <christiandevries8@gmail.com>
2020-12-15 15:57:00 +0100

    week 2

--- Week_2/traveltime.py
+++ Week_2/traveltime.py
@@ -2,19 +2,19 @@ distance_str = input("How many km do you have to travel from home to work?")
 
 distance_int = int(distance_str)
 
-walking_speed_kmh = 5
+walking_speed_kmh = float(input("What is your speed in km/h when walking?"))
 walking_going_min = 1
 walking_parking_min = 0
 
 walking_travel_time_min = walking_going_min + 60 * (distance_int / walking_speed_kmh) + walking_parking_min
 
-biking_speed_kmh = 12
+biking_speed_kmh = float(input("What is your speed in km/h when biking?"))
 biking_going_min = 3
 biking_parking_min = 2
 
 biking_travel_time_min = biking_going_min + 60 * (distance_int / biking_speed_kmh) + biking_parking_min
 
-driving_speed_kmh = 50
+driving_speed_kmh = float(input("What is your speed in km/h when driving?"))
 driving_going_min = 5
 driving_parking_min = 8
 
